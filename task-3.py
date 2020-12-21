13fa93ad0ad31f6972954101d830862dc11a2e8c
Max <m_gubanov@bk.ru>
2020-12-21 17:24:50 +0300

    n + nn + nnn

--- task-3.py
+++ task-3.py
@@ -1,3 +1,3 @@
 n = input('Введите значение "n": ')
-sum = int(n) + int((n + n)) + int((n + n + n))
+sum = int(n) + int((2 * n)) + int((3 * n))
 print(f'n + nn + nnn = {n} + {n}{n} + {n}{n}{n} = ', sum)
