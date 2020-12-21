8d9c34637b23ac05607571408fbf7850dc9112b1
Max <m_gubanov@bk.ru>
2020-12-21 15:42:25 +0300

    Расчет рентабельности фирмы

---
+++ task-5.py
@@ -0,0 +1,12 @@
+proceeds = int(input('Введите значение выручки фирмы: '))
+costs = int(input('Введите значение издержек фирмы: '))
+
+if proceeds > costs:
+    profit = proceeds - costs
+    print(f"Ваша фирма в прибыли и она составляет {profit} рублей.")
+    profitability = (profit / costs) * 100
+    print(f"Рентабельность фирмы {profitability:.0f}%.")
+    staff = int(input('Сколько сотрудников в вашей фирме? '))
+    print(f"Прибыль в рассчете на одного сотрудника составляет - {(profit / staff):.2f} рублей.")
+else:
+    print('Ваша фирма в убытке.')
