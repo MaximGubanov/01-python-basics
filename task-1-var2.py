831d200e0a58de0d00cc9b225d88966a46d7ee91
Max <m_gubanov@bk.ru>
2020-12-23 21:27:04 +0300

    Вариант с использованием цикла "for".

---
+++ task-1-var2.py
@@ -0,0 +1,3 @@
+database_list = ['book', 458, True, None, 'spoon', False, [1,2,3,5,5], dict(name = 'John')]
+for i in range(len(database_list)):
+    print(f"{database_list[i]} - {type(database_list[i])}")
