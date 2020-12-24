1ee307e68b0c38e4287e97ded083c80f077209bd
Max <m_gubanov@bk.ru>
2020-12-24 08:08:47 +0300

    Обмен значениями соседних эементов

--- task-1-var2.py
+++ task-1-var2.py
@@ -1,3 +1,3 @@
-database_list = ['book', 458, True, None, 'spoon', False, [1,2,3,5,5], dict(name = 'John')]
+database_list = ['book',45.7, 458, True, None, 'spoon', False, [1,2,3,5,5], dict(name = 'John')]
 for i in range(len(database_list)):
     print(f"{database_list[i]} - {type(database_list[i])}")
--- task-2.py
+++ task-2.py
@@ -1,21 +1,29 @@
-# database_list = ['book',45.7, 458, True, None, 'spoon']
-data_base = []
-user = None
+my_list = []
+new_list = []
+element = None
+last_element = None
+i = 0
+
 while True:
-    user = input('Создайте список, для завершения введите "q": ')
-    if user == 'q':
+    element = input('Создайте список, по окончании нажмите "q": ')
+    if element == 'q':
         print('Список готов.')
         break
     else:
-        data_base.append(user)
+        my_list.append(element)
 
-i = 0
-while i < len(data_base):
-    if len(data_base) % 2 == 0:
-        el_1, el_2 = data_base[i], data_base[i + 1]
-        el_1, el_2 = el_2, el_1
-        print(el_1, el_2)
-        i += 2
-    else:
-        end_elemenent = data_base.pop()
+while i < len(my_list):
+    if len(my_list) % 2 == 1:
+        last_element = my_list.pop()
         continue
+    else:
+        el_1, el_2 = my_list[i], my_list[i + 1]
+        el_1, el_2 = el_2, el_1
+        new_list.append(el_1)
+        new_list.append(el_2)
+        i += 2
+
+if last_element:
+    new_list.append(last_element)
+
+print(f"{my_list} - {new_list}")
