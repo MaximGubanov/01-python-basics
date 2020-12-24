b9a0f1b97773b732bef546d2f1806a516931bcac
Max <m_gubanov@bk.ru>
2020-12-24 08:19:13 +0300

    Реализация скрипта проверки типа данных. (Цикл While)

--- task-1.py
+++ task-1.py
@@ -1,5 +1,7 @@
 database_list = ['book',45.7, 458, True, None, 'spoon', False, [1,2,3,5,5], dict(name = 'John')]
+
 i = 0
-while len(database_list):
+
+while i != len(database_list):
     print(f"{database_list[i]} - {type(database_list[i])}")
     i += 1
